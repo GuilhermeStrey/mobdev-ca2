2217ccb31d3ff03b88e51943089b236212afe417
Guilherme Augusto Strey <guilherme.a.strey@gmail.com>
2020-05-18 16:26:26 +0000

    Adding camera.html and routing

--- mobdev-ca2/src/app/pages/tabs/tabs-routing.module.ts
+++ mobdev-ca2/src/app/pages/tabs/tabs-routing.module.ts
@@ -50,6 +50,15 @@ const routes: Routes = [
                         loadChildren: () => import('../deaths/deaths.module').then(m => m.DeathsPageModule)
                     }
                 ]
+            },
+            {
+                path: 'camera',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../camera/camera.module').then(m => m.CameraPageModule)
+                    }
+                ]
             }
         ]
     },
